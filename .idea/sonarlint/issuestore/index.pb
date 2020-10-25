b1b6a18badb00b6d66589e0329c6f1d31c98711e
Lito Talaba <LTalaba@Arcanys.com>
2020-10-25 19:57:45 +0800

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,12 @@
+
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+:
+
+src/App.js,1\e\1e43fa63aecd2127be034b3fb9d4caa172275714
+B
+docker-compose.yml,3\5\35b8c13cf2eb2a194eada000eb310d65aed53b2a
+A
+package-lock.json,f\a\fa288d1472d29beccb489a676f68739ad365fc47
+>
+Dockerfile.dev,5\0\5097cd467d1a52423d6921e6ce2f907ddec2e81c
--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit sdfsdfsd <code>src/App.js</code> and save to reloadqwqqweq.
+          </p>
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React ASDASDASDAS
         </a>
       </header>
     </div>
